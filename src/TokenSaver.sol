77a39a5a6b8847d708d80b89074a8018b9397499
Strapontin <antodu73@hotmail.fr>
2025-06-02 10:34:36 +0200

    Updated suggestions

--- src/TokenSaver.sol
+++ src/TokenSaver.sol
@@ -21,12 +21,15 @@ contract TokenSaver {
 
     TokenTracked[] tokenTracked;
 
-    modifier onlyOwner() {
-        require(msg.sender == address(this), "Not account onlyOwner");
+    modifier onlyEOA() {
+        if (msg.sender != address(this)) {
+            revert NotSmartWalletEOA(msg.sender, address(this));
+        }
         _;
     }
 
     error BalanceBelowMinimum(address token, uint256 minAmount, uint256 actualAmount);
+    error NotSmartWalletEOA(address sender, address eoa);
 
     /**
      * @notice Adds or updates a token to be tracked with a specified minimum amount.
@@ -34,7 +37,7 @@ contract TokenSaver {
      * @param _token The address of the token to track. Use address(0) for native token.
      * @param _minAmount The minimum amount of the token to maintain.
      */
-    function addOrUpdateTokenTracked(address _token, uint256 _minAmount) external onlyOwner {
+    function addOrUpdateTokenTracked(address _token, uint256 _minAmount) external onlyEOA {
         // Update the token if found
         for (uint256 i = 0; i < tokenTracked.length; i++) {
             if (tokenTracked[i].token == _token) {
@@ -50,7 +53,7 @@ contract TokenSaver {
      * @notice Removes a token from the tracking list.
      * @param _token The address of the token to remove. Use address(0) for native token.
      */
-    function removeToken(address _token) external onlyOwner {
+    function removeToken(address _token) external onlyEOA {
         uint256 listLength = tokenTracked.length;
 
         for (uint256 i = 0; i < listLength; i++) {
@@ -67,7 +70,7 @@ contract TokenSaver {
      * @dev Reverts if any token balance falls below the specified minimum amount.
      * @param calls An array of Call structs containing the details of each call to execute.
      */
-    function execute(Call[] calldata calls) external onlyOwner {
+    function execute(Call[] calldata calls) external onlyEOA {
         TokenTracked[] memory _tokenTracked = new TokenTracked[](tokenTracked.length);
 
         // Checks the value of tokens with minAmount == uint.max
@@ -82,7 +85,7 @@ contract TokenSaver {
         // Execute calls
         for (uint256 i = 0; i < calls.length; i++) {
             (bool success,) = calls[i].to.call{value: calls[i].value}(calls[i].data);
-                (success, "Call reverted");
+            require(success, "Call reverted");
         }
 
         // Revert if balances are not as expected
@@ -101,7 +104,7 @@ contract TokenSaver {
      * @return The balance of the specified token.
      */
     function _getTokenValue(address tokenAddress) private view returns (uint256) {
-        if (tokenAddress == address(0)) { 
+        if (tokenAddress == address(0)) {
             return address(this).balance;
         }
 
